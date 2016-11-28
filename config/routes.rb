bf66d558e0b4cc48db34df3e96020f77b6bc404f
tfreebern2 <tfreebern2@gmail.com>
2016-11-28 23:03:58 +0000

    User dashboard page

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,8 @@ Rails.application.routes.draw do
   resources :places do
     resources :comments, only: :create
   end
+  resources :users, only: :show
+  
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
